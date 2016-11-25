25428a6f0e81b66bc860bf41ea87d75da31c1b83
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-11-25 20:59:30 +0000

    ResponsiveUIUser  - started to implement

--- src/ResponsiveUIUser.java
+++ src/ResponsiveUIUser.java
@@ -1,10 +1,33 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author crkimberley on 25/11/2016.
  */
-public class ResponsiveUIUser {
+public class ResponsiveUIUser implements Runnable {
 
-    private ExecutorService executorService = new Executors.newCachedThreadPool();
+    private static int userNumber = 0, TASKS_PER_USER = 50, LONG_WAIT = 5000;
+    private static ResponsiveUIApp app = new ResponsiveUIApp();
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+    private int userID, waitThreshold;
+
+    public ResponsiveUIUser(int waitThreshold) {
+        this.waitThreshold = waitThreshold;
+        userID = ++userNumber;
+    }
+
+    public void run() {
+        Runnable task;
+        for (int taskID = 0; taskID < TASKS_PER_USER; taskID++) {
+            while (app.getMaxWait() > waitThreshold) {
+                System.out.println("User:" + userID + " says the is site down! They'll come back later");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(LONG_WAIT);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 }
